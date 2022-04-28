256591830b296327603f58a68d5cda25bd1a0234
exersalza <squidodle44@gmail.com>
2022-04-28 09:13:34 +0200

    added some stuff to learn

---
+++ learnC/w3schools/basics/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// dis is a comment
+/*
+ * dis is a multiline comment
+ *
+ */
+
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
---
+++ learnC/w3schools/basics/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // standart lib for in-n-output
+
+int main() {
+	// Var syntax:
+	// type varName = value;
+
+	int myNum = 5;
+	// or
+
+	int myNum2;
+	myNum2 = 5;
+
+	int myNum3;
+
+	printf("%d\n", myNum);
+}
