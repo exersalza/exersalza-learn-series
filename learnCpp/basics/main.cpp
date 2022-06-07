f77687c44224e91f4b696cd0518d52ed4be09385
exersalza <squidodle44@gmail.com>
2022-06-07 15:28:12 +0200

    added cpp

---
+++ learnCpp/basics/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by julian on 6/7/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int myNum = 15;
+    double largerNumber = 20000.000L;
+    float smolNumber;
+    smolNumber = 15.99;
+    char myLetter = 'D';
+    string myString = "Hello";
+    bool isTrue = true;
+
+    cout << "Hello world" << myString << endl;
+    return 0;
+}
