e70da137977e4826fa0d2f0a155b232f64a9b382
exersalza <squidodle44@gmail.com>
2022-10-24 13:20:26 +0200

    file reading

--- learnCpp/basics/main.cpp
+++ learnCpp/basics/main.cpp
@@ -3,14 +3,23 @@
 //
 
 #include <iostream>
-#include <memory>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
+
 int main() {
-    string name = "Mosh";
-
-
+    ifstream file;
+    string str;
+    file.open("../basics/data");
+    if (file.is_open()) {
+        while (!file.eof()) {
+            getline(file, str);
+            cout << str << endl;
+        }
+        file.close();
+    }
 
     return 0;
 }
